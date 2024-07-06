78bd4221d7ce7e8f72e28c1631fd6e3011b63b68
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2024-07-06 23:42:02 +0700

    [MODIFY] main.go

--- main.go
+++ main.go
@@ -8,6 +8,33 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
 )
 
+// Handler is the entry point for Vercel
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	//Create fiber
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		name := os.Getenv("NAME")
+		if name == "" {
+			name = "World"
+		}
+		return c.SendString("Hello " + name)
+	})
+
+	adaptor.FiberApp(app)(w, r)
+}
+
+func main() {
+	// Create a simple server
+	http.HandleFunc("/", Handler)
+
+	// Start the server on port 3000
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		panic(err)
+	}
+}
+
 // type User struct {
 // 	Name string `json:"name,omitempty"`
 // 	Age  int    `json:"age,omitempty"`
@@ -58,30 +85,3 @@ import (
 
 // 	return nil
 // }
-
-// Handler is the entry point for Vercel
-func Handler(w http.ResponseWriter, r *http.Request) {
-
-	//Create fiber
-	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		name := os.Getenv("NAME")
-		if name == "" {
-			name = "World"
-		}
-		return c.SendString("Hello " + name)
-	})
-
-	adaptor.FiberApp(app)(w, r)
-}
-
-func main() {
-	// Create a simple server
-	http.HandleFunc("/", Handler)
-
-	// Start the server on port 3000
-	if err := http.ListenAndServe(":3000", nil); err != nil {
-		panic(err)
-	}
-}
